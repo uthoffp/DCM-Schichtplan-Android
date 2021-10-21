902b6a6355db6302bad70f7705c1df3538bb3112
Philipp Uthoff <philipp.uthoff@gmx.de>
2021-10-21 13:30:34 +0200

    Simple API call

--- app/src/main/java/com/uthoff/dcm/android/repository/datasource/Webservice.kt
+++ app/src/main/java/com/uthoff/dcm/android/repository/datasource/Webservice.kt
@@ -1,4 +1,9 @@
 package com.uthoff.dcm.android.repository.datasource
 
+import android.telecom.Call
+import retrofit2.http.GET
+
 interface Webservice {
+    @GET("/")
+    fun getLandingPage(): retrofit2.Call<String>
 }
--- app/src/main/java/com/uthoff/dcm/android/view/MainActivity.kt
+++ app/src/main/java/com/uthoff/dcm/android/view/MainActivity.kt
@@ -2,13 +2,26 @@ package com.uthoff.dcm.android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
 import com.uthoff.dcm.android.R
+import com.uthoff.dcm.android.repository.datasource.Webservice
 import com.uthoff.dcm.android.view.fragments.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import com.google.gson.GsonBuilder
+
+import com.google.gson.Gson
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
         setFragment(PlannedFragment())
         navigationView.setNavigationItemSelectedListener { menuItem ->
-            when(menuItem.itemId) {
+            when (menuItem.itemId) {
                 R.id.abrequest -> setFragment(AbRequestFragment())
                 R.id.actual -> setFragment(ActualFragment())
                 R.id.clocking -> setFragment(ClockingFragment())
@@ -38,11 +51,34 @@ class MainActivity : AppCompatActivity() {
             drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
+
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.56.1:8080")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+        val service = retrofit.create(Webservice::class.java)
+        val call = service.getLandingPage()
+
+        call.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.code() == 200) {
+                    val i: Int = 0;
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                val i: Int = 0;
+            }
+        })
     }
 
-    fun AppCompatActivity.setFragment(fragment: Fragment){
+    fun AppCompatActivity.setFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.layout,fragment)
+        transaction.replace(R.id.layout, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
     }
